7850bc2ff07620bb0d20ab084f1c40a3eef3af3e
Vector <vector02web@gmail.com>
2024-11-11 16:21:25 +0100

    tranfer function commit

--- packages/foundry/src/Elig.sol
+++ packages/foundry/src/Elig.sol
@@ -7,6 +7,8 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Elig is ERC20, Ownable {
+    address[] public eligAddresses;
+
     constructor(
         string memory name,
         string memory symbol,
@@ -15,5 +17,17 @@ contract Elig is ERC20, Ownable {
         _mint(msg.sender, initialSupply * (10 ** decimals()));
     }
 
-    // function name() returns () {}
+    function transferTokens(
+        address[] calldata receivers,
+        uint256 amount
+    ) external onlyOwner {
+        eligAddresses = receivers;
+        uint256 totalAmount = receivers.length * amount;
+
+        require(balanceOf(msg.sender) >= totalAmount, "Not enough tokens!!");
+
+        for (uint256 i = 0; i < receivers.length; i++) {
+            _transfer(msg.sender, receivers[i], amount);
+        }
+    }
 }

37f1ed29eb61d77eada9f82f808461eed6e2438a
Vector <vector02web@gmail.com>
2024-11-17 13:53:28 +0100

    recent commit

--- packages/foundry/script/DeployElig.s.sol
+++ packages/foundry/script/DeployElig.s.sol
@@ -4,16 +4,28 @@ pragma solidity ^0.8.13;
 import {Script, console} from "forge-std/Script.sol";
 import {Elig} from "../src/Elig.sol";
 
-contract EligScript is Script {
+contract DeployElig is Script {
     Elig public elig;
 
-    function setUp() public {}
+    // function setUp() public {}
 
     function run() public {
+        // Let's load the environmental variables
+        string memory tokenName = vm.envString("Eligible");
+        string memory tokenSymbol = vm.envString("Elig");
+        uint256 initialSupply = vm.envUint("1000000000");
+
+        // Start braodcast on private key of deployer.
         vm.startBroadcast();
 
-        // elig = new Elig();
+        Elig elig = new Elig(tokenName, tokenSymbol, initialSupply);
 
         vm.stopBroadcast();
+        // Stop braodcast on private key of deployer.
+
+        // Let's log the deploying address
+        console.log("Deployed to:", address(elig));
+
+        // Okay, that should do it.
     }
 }
